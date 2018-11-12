73f4239c32db44c668c9ae9f7ef0311f14a0a59c
malcolmobimpe <malcolmobimpe@gmail.com>
2018-11-11 20:59:04 -0500

    all files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Route, Link, Switch} from "react-router-dom";
+import Dashboard from './components/dashboard/Dashboard'
+import RecipeDetails from "./components/recipes/RecipeDetails";
+import SignIn from "./components/auth/SignIn";
+import SignUp from "./components/auth/SignUp";
+import CreateRecipe from "./components/recipes/CreateRecipe"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Dashboard}/>
+            <Route path='/recipes/:id' component={RecipeDetails} />
+            <Route path='/signin' component={SignIn} />
+            <Route path='/signup' component={SignUp} />
+            <Route path='/create' component={CreateRecipe} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
